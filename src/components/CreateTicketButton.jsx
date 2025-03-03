fb48cd350b09b4d51754c903c6a2bb88e770c1e8
Abi McCullough <abi.mccullough@gmail.com>
2025-03-03 12:28:56 +0000

    Description fix

--- src/components/CreateTicketButton.jsx
+++ src/components/CreateTicketButton.jsx
@@ -8,14 +8,14 @@ const CreateTicketButton = ({ companyName, slogan, icon, navigateTo, fontFamily,
     const taskData = {
       taskName: `Logo Generator - ${companyName}`, // Example task name
       taskDescription: `${generatedURL}
-      Icon style: ${icon.id}
-      Company Name: ${companyName}
-      Slogan: ${slogan}
-      Font Family: ${fontFamily || 'default'}
-      Font Colour: ${fontColour || 'default'}
-      Accent Colour: ${accentColour || 'default'}
-      Font Size: ${logoFontSize || 'default'}
-      Font Spacing: ${logoFontSpacing || 'default'}
+Logo Style: Logo ${icon.id}
+Company Name: ${companyName}
+Slogan: ${slogan}
+Font Family: ${fontFamily || 'default'}
+Font Colour: ${fontColour || 'default'}
+Accent Colour: ${accentColour || 'default'}
+Font Size: ${logoFontSize || 'default'}
+Font Spacing: ${logoFontSpacing || 'default'}
       `, 
     };
   
